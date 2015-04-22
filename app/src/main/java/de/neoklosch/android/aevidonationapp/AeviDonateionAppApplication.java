88daa75017c13d50312c760a33cbe9d911c3c8e8
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 18:24:41 +0200

    Added app aplication.

---
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/AeviDonateionAppApplication.java
@@ -0,0 +1,23 @@
+package de.neoklosch.android.aevidonationapp;
+
+import android.app.Application;
+
+
+public class AeviDonateionAppApplication extends Application {
+    private static volatile AeviDonateionAppApplication instance;
+
+    /**
+     * Returns the singleton instance.
+     *
+     * @return the singleton instance
+     */
+    public static AeviDonateionAppApplication getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+}
