d7ca39a63d5c25e84222c771dd88f6b5a69909ee
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 17:14:56 +0200

    Initial app setup.

---
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/MainActivity.java
@@ -0,0 +1,61 @@
+package de.neoklosch.android.aevidonationapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.aevi.payment.PaymentRequest;
+import com.aevi.payment.TransactionResult;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View view) {
+        PaymentRequest payment = new PaymentRequest(new BigDecimal("20.00"));
+        payment.setCurrency(Currency.getInstance("EUR"));
+        startActivityForResult(payment.createIntent(), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TransactionResult result = TransactionResult.fromIntent(data);
+        Toast.makeText(this, "Transaction result: " + result.getTransactionStatus(), Toast.LENGTH_SHORT).show();
+
+    }
+}
