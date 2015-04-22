6f6323ac6e9b6e4af7a6672beadc2ec1fc28b480
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 23:22:11 +0200

    Added the shared preferences settings to the specific fields.

--- app/src/main/java/de/neoklosch/android/aevidonationapp/activities/AmountChooserActivity.java
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/activities/AmountChooserActivity.java
@@ -3,6 +3,7 @@ package de.neoklosch.android.aevidonationapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.aevi.payment.PaymentRequest;
@@ -14,6 +15,7 @@ import java.util.Currency;
 
 import de.neoklosch.android.aevidonationapp.Constants;
 import de.neoklosch.android.aevidonationapp.R;
+import de.neoklosch.android.aevidonationapp.SharedPreferencesHelper;
 
 public class AmountChooserActivity extends BaseActivity {
     @Override
@@ -21,10 +23,14 @@ public class AmountChooserActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amount_chooser);
 
+        EditText amountValue = (EditText) findViewById(R.id.amount_chooser_activity_value);
+        amountValue.setText("" + SharedPreferencesHelper.getFloat(AmountChooserActivity.this, Constants.SHARED_PREFERENCES_KEY_DEFAULT_AMOUNT, 10.0f));
+
         findViewById(R.id.amount_chooser_activity_button_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PaymentRequest payment = new PaymentRequest(new BigDecimal("20.00"));
+                EditText amountValue = (EditText) findViewById(R.id.amount_chooser_activity_value);
+                PaymentRequest payment = new PaymentRequest(new BigDecimal(amountValue.getText().toString()));
                 payment.setCurrency(Currency.getInstance("EUR"));
                 startActivityForResult(payment.createIntent(), Constants.PAYMENT_REQUEST_CODE);
             }
--- app/src/main/java/de/neoklosch/android/aevidonationapp/activities/MainActivity.java
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/activities/MainActivity.java
@@ -3,10 +3,13 @@ package de.neoklosch.android.aevidonationapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.skyfishjy.library.RippleBackground;
 
+import de.neoklosch.android.aevidonationapp.Constants;
 import de.neoklosch.android.aevidonationapp.R;
+import de.neoklosch.android.aevidonationapp.SharedPreferencesHelper;
 
 
 public class MainActivity extends BaseActivity {
@@ -15,6 +18,13 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView headline = (TextView) findViewById(R.id.donate_view_headline);
+        TextView description = (TextView) findViewById(R.id.donate_view_description);
+
+        headline.setText(SharedPreferencesHelper.getString(MainActivity.this, Constants.SHARED_PREFERENCES_KEY_CHARITY_NAME, getString(R.string.charity_name_default)));
+        description.setText(SharedPreferencesHelper.getString(MainActivity.this, Constants.SHARED_PREFERENCES_KEY_DESCRIPTION, getString(R.string.charity_description_default)));
+
         findViewById(R.id.donate_activity_button_donate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
