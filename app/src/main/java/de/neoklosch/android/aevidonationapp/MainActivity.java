85c5a70a8cebcd9d0f106965ba21b23efa0047cf
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 20:56:42 +0200

    Removed the depricated ActionBarActivity and replaced them by the new AppCompatActivity.

--- app/src/main/java/de/neoklosch/android/aevidonationapp/MainActivity.java
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/MainActivity.java
@@ -1,8 +1,8 @@
 package de.neoklosch.android.aevidonationapp;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,7 +16,7 @@ import java.math.BigDecimal;
 import java.util.Currency;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
