ca7b2a60f8c1066d89145e8e8ca9f11bab2ec217
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 20:58:18 +0200

    Redirects the user to the amount chooser activity if he press on the donate button in the main activity.

--- app/src/main/java/de/neoklosch/android/aevidonationapp/MainActivity.java
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/MainActivity.java
@@ -3,17 +3,7 @@ package de.neoklosch.android.aevidonationapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.aevi.payment.PaymentRequest;
-import com.aevi.payment.TransactionResult;
-
-import java.math.BigDecimal;
-import java.util.Currency;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,17 +15,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.donate_activity_button_donate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PaymentRequest payment = new PaymentRequest(new BigDecimal("20.00"));
-                payment.setCurrency(Currency.getInstance("EUR"));
-                startActivityForResult(payment.createIntent(), 0);
+                Intent amountChooserIntent = new Intent(MainActivity.this, AmountChooser.class);
+                startActivity(amountChooserIntent);
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        TransactionResult result = TransactionResult.fromIntent(data);
-        Toast.makeText(this, "Transaction result: " + result.getTransactionStatus(), Toast.LENGTH_SHORT).show();
-
     }
 }
