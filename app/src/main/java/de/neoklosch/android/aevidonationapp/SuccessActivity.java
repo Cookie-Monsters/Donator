ac7806514b8cee0c275cd68cc3007a091e6a9af8
Markus Paeschke <markus.paeschke@gmail.com>
2015-04-22 21:47:22 +0200

    Close the app after 20 seconds and clear activity stack.

--- app/src/main/java/de/neoklosch/android/aevidonationapp/SuccessActivity.java
+++ app/src/main/java/de/neoklosch/android/aevidonationapp/SuccessActivity.java
@@ -1,18 +1,18 @@
 package de.neoklosch.android.aevidonationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
 
 public class SuccessActivity extends AppCompatActivity {
     final Handler handler = new Handler();
     final Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            Toast.makeText(SuccessActivity.this, "check", Toast.LENGTH_SHORT).show();
-            handler.postDelayed(this, 2000);
+            closeActivity();
         }
     };
 
@@ -21,12 +21,26 @@ public class SuccessActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
 
-        handler.postDelayed(runnable, 1500);
+        findViewById(R.id.success_activity_button_finish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeActivity();
+            }
+        });
+
+        handler.postDelayed(runnable, 20000);
     }
 
     @Override
     protected void onPause() {
         handler.removeCallbacks(runnable);
         super.onPause();
+    }
+
+    protected void closeActivity() {
+        Intent mainActivityIntent = new Intent(SuccessActivity.this, MainActivity.class);
+        mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(mainActivityIntent);
+        finish();
     }
 }
